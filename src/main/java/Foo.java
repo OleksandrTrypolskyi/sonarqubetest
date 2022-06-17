80ef30ac960a13ec1108954a7831797b82d0b62f
TrypolskyiS <tripolskys@gmail.com>
2022-06-17 14:09:06 +0300

    Initial commit
    Added maven. Added two classes in order to test sonarqube.

---
+++ src/main/java/Foo.java
@@ -0,0 +1,5 @@
+public class Foo {
+    public static void foo() {
+        System.out.println("Repeated code");
+    }
+}
---
+++ src/main/java/org/example/Foo.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Foo {
+    public static void foo() {
+        System.out.println("Repeated code");
+    }
+}
